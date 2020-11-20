b073074d13f5ece308019b3e0075713374f32b0a
Yehezkiel Gunawan <yehezkiel.gunawan@codify.id>
2020-11-20 16:14:56 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Button, Container, Divider, Header, Segment } from "semantic-ui-react";
+import "./App.css";
 
 function App() {
+  const [minutes, setMinutes] = useState(3);
+  const [second, setSecond] = useState(0);
+
+  useEffect(() => {
+    const myInterval = setInterval(() => {
+      if (second > 0) {
+        setSecond(second - 1);
+      }
+
+      if (second === 0) {
+        if (minutes === 0) {
+          clearInterval(myInterval);
+        } else {
+          setMinutes(minutes-1);
+          setSecond(59);
+        }
+      }
+    }, 1000);
+
+    return () => clearInterval(myInterval);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container textAlign="center" className="container">
+        <Segment>
+          <Header size="huge">Countdown Timer</Header>
+          <h3>
+            {minutes} : {second < 10 ? `0${second}` : second}
+          </h3>
+          <Divider></Divider>
+          <Button color="green">Start</Button>
+          <Button color="black">Reset</Button>
+        </Segment>
+      </Container>
     </div>
   );
 }
