82660a5180c5b7627dbf391ab25639dc456cca3b
Yehezkiel Gunawan <yehezkiel.gunawan@student.umn.ac.id>
2020-11-27 15:30:10 +0700

    Add finish logic

--- src/App.js
+++ src/App.js
@@ -7,12 +7,21 @@ function App() {
   const [second, setSecond] = useState(0);
   const [flagPause, setFlagPause] = useState(false);
   const [flagStart, setFlagStart] = useState(false);
+  const [flagFinish, setFlagFinish] = useState(false);
 
   const resetCountdown = () => {
     setMinutes(0);
     setSecond(0);
     setFlagPause(false);
     setFlagStart(false);
+    setFlagFinish(false);
+  };
+
+  const changeFlagFinish = () => {
+    if (flagFinish === false) {
+      return setFlagFinish(true);
+    }
+    return setFlagFinish(false);
   };
 
   const changeFlagPause = () => {
@@ -69,6 +78,7 @@ function App() {
         if (second === 0) {
           if (minutes === 0) {
             setFlagStart(false);
+            setFlagFinish(true);
             clearInterval(myInterval);
           } else {
             setMinutes(minutes - 1);
@@ -146,37 +156,68 @@ function App() {
         </Container>
       </div>
     );
+  } else {
+    if (flagFinish === true) {
+      return (
+        <div className="App">
+          <Container textAlign="center" className="container">
+            <Segment.Group>
+              <Segment inverted>
+                <Header size="huge">Countdown Timer</Header>
+              </Segment>
+              <Segment>
+                <h3>
+                  <Header size="medium">BOOMMM!!!!</Header>
+                </h3>
+                <h3>
+                  {minutes < 10 ? `0${minutes}` : minutes} :{" "}
+                  {second < 10 ? `0${second}` : second}
+                </h3>
+                <h3>
+                  <Icon name="clock outline"></Icon>
+                </h3>
+              </Segment>
+              <Segment inverted>
+                <Button color="green" onClick={startCountdown}>
+                  Start Again
+                </Button>
+              </Segment>
+            </Segment.Group>
+          </Container>
+        </div>
+      );
+    }
+    return (
+      <div className="App">
+        <Container textAlign="center" className="container">
+          <Segment.Group>
+            <Segment inverted>
+              <Header size="huge">Countdown Timer</Header>
+            </Segment>
+            <Segment>
+              <h3>
+                <Icon name="angle up" onClick={plusMinutes}></Icon>{" "}
+                <Icon name="angle up" onClick={plusSecond}></Icon>
+              </h3>
+              <h3>
+                {minutes < 10 ? `0${minutes}` : minutes} :{" "}
+                {second < 10 ? `0${second}` : second}
+              </h3>
+              <h3>
+                <Icon name="angle down" onClick={minusMinutes}></Icon>{" "}
+                <Icon name="angle down" onClick={minusSeconds}></Icon>
+              </h3>
+            </Segment>
+            <Segment inverted>
+              <Button color="green" onClick={startCountdown}>
+                Start
+              </Button>
+            </Segment>
+          </Segment.Group>
+        </Container>
+      </div>
+    );
   }
-  return (
-    <div className="App">
-      <Container textAlign="center" className="container">
-        <Segment.Group>
-          <Segment inverted>
-            <Header size="huge">Countdown Timer</Header>
-          </Segment>
-          <Segment>
-            <h3>
-              <Icon name="angle up" onClick={plusMinutes}></Icon>
-              <Icon name="angle up" onClick={plusSecond}></Icon>
-            </h3>
-            <h3>
-              {minutes < 10 ? `0${minutes}` : minutes} :{" "}
-              {second < 10 ? `0${second}` : second}
-            </h3>
-            <h3>
-              <Icon name="angle down" onClick={minusMinutes}></Icon>
-              <Icon name="angle down" onClick={minusSeconds}></Icon>
-            </h3>
-          </Segment>
-          <Segment inverted>
-            <Button color="green" onClick={startCountdown}>
-              Start
-            </Button>
-          </Segment>
-        </Segment.Group>
-      </Container>
-    </div>
-  );
 }
 
 export default App;
